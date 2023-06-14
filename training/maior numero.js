8e9215eae1b4e24562ed6976ef1ebb3ca648b473
Ryan Silva <ryansilvaalmada2@gmail.com>
2023-06-14 15:26:16 -0300

    deletado um arquivo e modificado o nome de outro

--- training/maior numero.js
+++
@@ -1,7 +0,0 @@
-
-        console.log(maxValue(7, 9))
-
-        function maxValue(n1 = 0, n2 = 0) {
-            return n1 > n2 ? n1 : n2;
-        }
-
---
+++ training/teste.js
@@ -0,0 +1,3 @@
+const maiorNumber = (n1, n2) => {return n1 < n2 ? n2 : n1};
+
+console.log(maiorNumber(5, 7));
